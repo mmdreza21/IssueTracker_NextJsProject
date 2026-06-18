b50b0751cc2e1c42e5a826d841a107d8ffbb29f9
mmdreza21 <mmdrez954@gmail.com>
2026-06-18 12:11:25 +0330

    feat: Integrate assignee selection in issue detail page
    - Add AssigneeSelect component to issue detail view
    - Display current assignee information
    - Pass issue ID and current assignee to select component

--- src/app/issues/[id]/page.tsx
+++ src/app/issues/[id]/page.tsx
@@ -4,12 +4,16 @@ import { notFound } from "next/navigation";
 import EditIssueButton from "./EditIssueButton";
 import IssueDetail from "./IssueDetail";
 import DeleteIssueButton from "./DeleteIssueButton";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/auth/authOption";
+import AssigneeSelect from "./assigneeSelect";
 
 interface Props {
   params: { id: string };
 }
 
 async function IssueDetailPage({ params }: Props) {
+  const session = await getServerSession(authOptions);
   const { id } = await params;
   if (typeof +id !== "number") notFound();
   const issue = await prisma.issue.findUnique({ where: { id: +id } });
@@ -20,12 +24,15 @@ async function IssueDetailPage({ params }: Props) {
       <Box className="md:col-span-4">
         <IssueDetail issue={issue} />
       </Box>
-      <Box>
-        <Flex direction="column" gap="4">
-          <EditIssueButton issueId={issue.id} />
-          <DeleteIssueButton issueId={issue.id} />
-        </Flex>
-      </Box>
+      {session && (
+        <Box>
+          <Flex direction="column" gap="4">
+            <AssigneeSelect issueId={issue.id} />
+            <EditIssueButton issueId={issue.id} />
+            <DeleteIssueButton issueId={issue.id} />
+          </Flex>
+        </Box>
+      )}
     </Grid>
   );
 }
