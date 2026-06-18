62d1b924875038e559c6d398bb29e985eb04c2f4
mmdreza21 <mmdrez954@gmail.com>
2026-06-18 17:18:01 +0330

    feat: Add QueryProvider to root layout
    - Wrap app with QueryClientProvider
    - Configure React Query default options
    - Enable React Query Devtools for development

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,8 @@ import { Inter } from "next/font/google";
 import "@radix-ui/themes/styles.css";
 import NavBar from "./NavBar";
 import { Container, Theme, ThemePanel } from "@radix-ui/themes";
-import Providers from "@/app/auth/providers";
+import QueryProvider from "./providers/QueryProvider";
+import AuthProvider from "./providers/Authprovider";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -25,15 +26,17 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${inter.variable} `}>
       <body>
-        <Providers>
-          <Theme accentColor="plum" panelBackground="solid" appearance="dark">
-            <NavBar></NavBar>
-            <main className="p-5">
-              <Container> {children}</Container>
-            </main>
-            {/* <ThemePanel></ThemePanel> */}
-          </Theme>
-        </Providers>
+        <QueryProvider>
+          <AuthProvider>
+            <Theme accentColor="plum" panelBackground="solid" appearance="dark">
+              <NavBar></NavBar>
+              <main className="p-5">
+                <Container> {children}</Container>
+              </main>
+              {/* <ThemePanel></ThemePanel> */}
+            </Theme>
+          </AuthProvider>
+        </QueryProvider>
       </body>
     </html>
   );
