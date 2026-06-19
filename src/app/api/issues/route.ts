2906edaca84510772ebe1f0212ce9b05c9b54973
mmdreza21 <mmdrez954@gmail.com>
2026-06-19 19:33:35 +0330

    Get Api added just for learning reasons

--- src/app/api/issues/route.ts
+++ src/app/api/issues/route.ts
@@ -4,6 +4,7 @@ import { prisma } from "@/lib/prisma";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/auth/authOption";
 import { Seed } from "../seed";
+import { Status } from "@prisma/client";
 
 export async function POST(request: NextRequest) {
     const session = getServerSession(authOptions)
@@ -11,14 +12,32 @@ export async function POST(request: NextRequest) {
 
     const body = await request.json()
 
-    Seed()
+    // Seed()
     const validation = createIssueSchema.safeParse(body)
     if (!validation.success)
         return NextResponse.json(validation.error.errors, { status: 400 })
 
     const newIssue = await prisma.issue.create({ data: { title: body.title, description: body.description } })
     return NextResponse.json(newIssue);
-
 }
 
+
+export async function GET(request: NextRequest, { searchParams }: { searchParams: { status: Status, } }) {
+
+    const statusParam = request.nextUrl.searchParams.get("status");
+
+    const where =
+        statusParam && Object.values(Status).includes(statusParam as Status)
+            ? { status: statusParam as Status }
+            : {};
+
+    const issues = await prisma.issue.findMany({
+        where,
+    });
+    if (!issues) return NextResponse.json({ error: "Invalid Issue" }, { status: 404 })
+
+
+    return NextResponse.json(issues);
+
+}
 
