0c7b666af4f83462e2b8958a70b22ec8ed99d172
mmdreza21 <mmdrez954@gmail.com>
2026-06-29 11:09:13 +0330

    feat: Implement dashboard page with analytics
    - Integrate IssueChart, IssueSummary, and LatestIssue components
    - Create grid layout for dashboard widgets
    - Add loading states for data fetching
    - Display real-time issue statistics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,31 @@
-import { Button, Flex } from "@radix-ui/themes";
-import { Spinner } from "./components";
+import { prisma } from "@/lib/prisma";
+import { Flex, Grid } from "@radix-ui/themes";
+import IssueSummary from "./IssueSummary";
+import LatestIssue from "./LatestIssue";
+import IssueChart from "./IssueChart";
+import { Metadata } from "next";
 
-export default function Home() {
+export default async function Home() {
+  const open = await prisma.issue.count({ where: { status: "OPEN" } });
+  const close = await prisma.issue.count({ where: { status: "CLOSED" } });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+
+  const statusProps = { open, close, inProgress };
+
   return (
-    <Button className="">
-      <span>Take NOTE THEN CODE</span>
-      <Spinner />
-    </Button>
+    <Grid gap={"5"} columns={{ initial: "1", md: "2" }}>
+      <Flex gap={"5"} direction="column">
+        <IssueSummary {...statusProps} />
+        <IssueChart {...statusProps} />
+      </Flex>
+      <LatestIssue />
+    </Grid>
   );
 }
+
+export const metadata: Metadata = {
+  title: "Issue Tracker - Dashboard",
+  description: "View a summary of Games issue!",
+};
